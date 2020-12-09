0f60cb6ca07f7f132eddd806a21481564a64de9c
zxinem <56019192+zxinem@users.noreply.github.com>
2020-12-09 22:51:07 +0800

    Add files via upload

--- view/admin/install/install_1.php
+++ view/admin/install/install_1.php
@@ -2,17 +2,18 @@
 
 <?php view::begin('content');?>
 
-<?php $default_client_id = 'ea2b36f6-b8ad-40be-bc0f-e5e4a4a7d4fa';?>
-<?php $default_client_secret = 'h27zG8pr8BNsLU0JbBh5AOznNS5Of5Y540l/koc7048=';?>
-
+<?php $default_client_id = '';?>
+<?php $default_client_secret = '';?>
+	
 <div class="mdui-container-fluid">
 	<div class="mdui-typo">
-	  <h1>系统安装 <small>设置应用ID和Secret</small></h1>
+	  <h1>程序安装 <small>应用ID和机密</small></h1>
 	</div>
 
 	<div class="mdui-typo">
       <h4 class="doc-article-title">
-	    填入<code>client_id</code>和<code>client_secret</code>
+	    填入<code>client_id</code>和<code>client_secret</code>,
+      	<a href="<?php echo $app_url;?>" target="_blank" class="mdui-btn mdui-color-theme-accent mdui-ripple"><i class="mdui-icon material-icons">&#xe89e;</i> 获取应用ID和机密(分两个页面显示，请注意保存)</a>
       </h4>
     </div>
 
@@ -20,24 +21,22 @@
 		<div class="mdui-textfield mdui-textfield-floating-label">
 			<i class="mdui-icon material-icons">https</i>
 			<label class="mdui-textfield-label">应用机密(client secret)</label>
- 			<?php if(config('client_secret')) { $client_secret_value = config('client_secret');} else {$client_secret_value = $default_client_secret;};?>
- 			<input type="text" type="text" class="mdui-textfield-input" name="client_secret" required value="<?php echo $client_secret_value;?>"/>
+			<input type="text" type="text" class="mdui-textfield-input" name="client_secret" required value="<?php echo config('client_secret');?>"/>
 			<div class="mdui-textfield-error">应用机密不能为空</div>
 		</div>
 		<br>
 		<div class="mdui-textfield mdui-textfield-floating-label">
 		  	<i class="mdui-icon material-icons">&#xe5c3;</i>
 		  	<label class="mdui-textfield-label">应用 ID(client_id)</label>
- 			<?php if(config('client_id')) { $client_id_value = config('client_id');} else {$client_id_value = $default_client_id;};?>
- 		  	<input type="text" class="mdui-textfield-input" name="client_id" required value="<?php echo $client_id_value;?>"/>
+		  	<input type="text" class="mdui-textfield-input" name="client_id" required value="<?php echo config('client_id');?>"/>
 		  	<div class="mdui-textfield-error">应用 ID不能为空</div>
 		</div>
 		<br>
 
 		<div class="mdui-textfield mdui-textfield-floating-label">
 		   <i class="mdui-icon material-icons">&#xe41a;</i>
- 		   <?php if($redirect_uri != 'http://localhost/'):?>
- 		   <label class="mdui-textfield-label">由于你的网站不是 <b>http://localhost/</b> , 将进行中转授权.</label>
+		   <?php if($redirect_uri == 'https://ju.tn/'):?>
+		   <label class="mdui-textfield-label">由于你的网站不是<b>http://localhost/</b>。将通过ju.tn进行中转</label>
 		   <?php endif;?>
 		   <label class="mdui-textfield-label"><?php echo $redirect_uri;?></label>
 		   <input type="text" class="mdui-textfield-input" disabled  value="<?php echo $redirect_uri;?>"/>
